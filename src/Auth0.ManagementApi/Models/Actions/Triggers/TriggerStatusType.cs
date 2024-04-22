4a53805ea195dd8829f6148e6222c53a18ffdbbe
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2024-04-22 00:02:06 -0400

    Missed JSON hints on TriggerStatusType

--- src/Auth0.ManagementApi/Models/Actions/Triggers/TriggerStatusType.cs
+++ src/Auth0.ManagementApi/Models/Actions/Triggers/TriggerStatusType.cs
@@ -1,5 +1,14 @@
-﻿namespace Auth0.ManagementApi.Models.Actions
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Auth0.ManagementApi.Models.Actions
 {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter), typeof(KebabCaseNamingStrategy))]
     public enum TriggerStatusType
     {
 
@@ -14,4 +23,5 @@
         Deprecated
 
     }
+
 }
