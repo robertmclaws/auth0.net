ecf435d617e8d92ef8f3f9c290b2ae3d6bb22a37
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2024-05-01 00:19:29 -0400

    Make it easier to force an Action deletion

--- src/Auth0.ManagementApi/Models/Actions/Requests/DeleteActionRequest.cs
+++ src/Auth0.ManagementApi/Models/Actions/Requests/DeleteActionRequest.cs
@@ -10,10 +10,28 @@ namespace Auth0.ManagementApi.Models.Actions
     [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     public class DeleteActionRequest
     {
+
+        #region Properties
+
         /// <summary>
         /// Force action deletion detaching bindings
         /// </summary>
         public bool? Force { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="DeleteActionRequest" /> class.
+        /// </summary>
+        /// <param name="force"></param>
+        public DeleteActionRequest(bool? force = null)
+        {
+            Force = force;
+        }
+
+        #endregion
 
     }
 
